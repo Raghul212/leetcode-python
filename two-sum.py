586f34db1910a1071d7f43c979fa47af67f77b33
Raghul212 <raghulramki21@gmail.com>
2023-10-22 20:23:03 +0530

    two-sum-python-implementation

---
+++ two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result=[0,0]
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    result[0]=i
+                    result[1]=j
+                    return result
